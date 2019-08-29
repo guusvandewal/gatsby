5a93dc75e9f427b71d599aeab5d053e2e08bf4f9
Guus van de Wal <git@guusvandewal.nl>
2019-08-29 15:51:20 +0200

    Up

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,17 @@ import { Link, graphql } from "gatsby"
 import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
+import { Helmet } from "react-helmet"
 
 export default ({ data }) => {
   console.log(data)
   return (
     <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <div>
         <h1
           css={css`
